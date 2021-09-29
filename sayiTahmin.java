0c1630f0baae9777781420ec78ca2f2aaf63889b
ersenremzi <88109467+ersenremzi@users.noreply.github.com>
2021-09-29 10:30:00 -0200

    Add files via upload

---
+++ sayiTahmin.java
@@ -0,0 +1,31 @@
+package bolum5_soru_cozumleri;
+
+import java.util.Scanner;
+
+public class soru11 {
+
+    public static void main(String[] args) {
+
+        Scanner tara = new Scanner(System.in);
+
+        int uretilenSayi = (int) (Math.random() * 101); // o ile 100 arası sayı üretilir.100 dahil.
+        int tahmin = -1 ;
+        int tahminEtmeSayisi = 0;
+
+        while (tahmin != uretilenSayi){
+
+            System.out.println("Tahmininizi giriniz :");
+            tahmin = tara.nextInt();
+            tahminEtmeSayisi++;
+
+            if (tahmin == uretilenSayi){
+                System.out.println("Tebrikler ! " +tahminEtmeSayisi + " seferde sayıyı buldunuz :)");
+            }else if (tahmin > uretilenSayi){
+                System.out.println("Tahmin ettiğiniz sayı yüksek,düşürün!");
+            }else {
+                System.out.println("Tahmin ettiğiniz sayı çok düşük,yükseltin.");
+            }
+
+        }
+    }
+}
